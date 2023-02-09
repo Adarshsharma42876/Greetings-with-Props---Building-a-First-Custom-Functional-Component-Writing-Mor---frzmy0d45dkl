613ec01d2c0db8dc18dbdbb978ed0eaed555d2d1
ADARSH SHARMA <120009672+Adarshsharma42876@users.noreply.github.com>
2023-02-09 12:54:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import '../styles/App.css';
 import Welcome from './Welcome';
 
 const App = () => {
-  let name = "Anand Dubey";
+  let name = "Adarsh sharma";
   return (
     <Welcome name={name} />
   )
